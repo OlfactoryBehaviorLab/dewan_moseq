afb7a23f34c5f2f1a9d38743d660dc3c6e67515a
Austin Pauley <austin2118ace@gmail.com>
2026-02-19 13:05:34 -0500

    parse moseq h5 file

--- dewan_moseq/readh5.py
+++ dewan_moseq/readh5.py
@@ -1,5 +1,26 @@
+import logging
 import pathlib
 import h5py
-import pandas
+import pandas as pd
 
+logger = logging.getLogger(__name__)
+keys = ['centroid', 'heading', 'latent_state', 'syllable']
 
+def readh5(path: pathlib.Path) -> dict[str, pd.DataFrame]:
+    if not path.exists() or not path.is_file():
+        raise FileNotFoundError(f"File not found: {path}")
+
+    all_data = {}
+
+    with h5py.File(path, "r") as f:
+        for experiment in f.keys():
+            animal_df = pd.DataFrame(columns=keys)
+            for key in keys:
+                raw_data = f[experiment][key][:]
+                if len(raw_data.shape) > 1:
+                    _data = [tuple(data) for data in raw_data]
+                    animal_df[key] = _data
+                else:
+                    animal_df[key] = raw_data
+            all_data[experiment] = animal_df
+        return all_data
