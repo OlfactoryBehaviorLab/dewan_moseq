f13fc36ba808b2c2fa80aa574fabdab1b61979d8
Austin Pauley <austin2118ace@gmail.com>
2026-02-20 13:11:00 -0500

    Decode key into constituent parts

--- dewan_moseq/readh5.py
+++ dewan_moseq/readh5.py
@@ -24,3 +24,23 @@ def readh5(path: pathlib.Path) -> dict[str, pd.DataFrame]:
                     animal_df[key] = raw_data
             all_data[experiment] = animal_df
         return all_data
+
+
+def decode_trial_name(key_name: str) -> tuple[str, str, int]:
+    """
+    Decode trial key into its constituent pieces
+
+    key is in the form [Animal]-[Experiment]-trial-[Trial_Number]DLC-[DLC_MODEL_INFORMATION]
+    Parameters
+    ----------
+    key_name : str
+        Trial key name
+
+    Returns
+    -------
+        Parsed String: tuple[str, str, int]
+            index 0: Animal, index 1: Experiment, index 2: Trial_Number
+    """
+    pieces = key_name.split("DLC")[0].split("-")
+
+    return pieces[0], pieces[1], int(pieces[3])
